adb74829371dac9c9eff3b6019b5302d250847b6
GAKers2002 <144271532+GAKers2002@users.noreply.github.com>
2023-11-01 14:52:07 -0400

    Add files via upload

--- InverseDocumentFrequencyCalculator.java
+++ InverseDocumentFrequencyCalculator.java
@@ -1,5 +1,7 @@
 package Lab6;
 
+import java.util.Iterator;
+import java.util.Set;
 import java.util.TreeMap;
 
 public class InverseDocumentFrequencyCalculator implements IInverseFreq {
@@ -14,6 +16,7 @@ public class InverseDocumentFrequencyCalculator implements IInverseFreq {
             String lyric = songLyrics.get(songTitle);
             String[] words = lyric.split(" ");
             
+            
             // Calculate the Inverse Document Frequency (IDF) Values here
             // You might need to introduce other methods and additional variables
             
@@ -24,9 +27,36 @@ public class InverseDocumentFrequencyCalculator implements IInverseFreq {
             ////////////////////////////
             //WRITE HERE
             //////////////////////////
-        }
-        
+            int n = 0;
+            int N = songNum(songLyrics);
+            Set<String> songs = songLyrics.keySet();
+            Iterator<String> itr = songs.iterator();
+            //find number of times X is repeated; iterates through list of words
+            for(int i = 0; i < words.length; i++) {
+                //iterates through all songs in treemap
+                for(int j = 0; j < N; j++) {
+                    if(itr.next().contains(words[i])) {
+                        n++;
+                    }
+                }
+                //find frequency by breaking up equation into smaller parts
+                double innerE = (N - n + 0.5)/(n + 0.5) + 1;
+                double outerE = Math.log(innerE);
+                //add to result tree
+                result.put(words[i], outerE);
+            }          
+        }  
         return result;
     }
-
+    
+    //find number of songs (N)
+    public static int songNum(TreeMap<String, String> songLyrics) {
+        int count = 0;
+        if(!songLyrics.isEmpty()) {
+            for(int i = 0; i < songLyrics.size(); i++) {
+                count++;
+            }
+        } 
+        return count;
+    }
 }
